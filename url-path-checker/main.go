39e1e07a7c561bcba2221630bb6139c93a6d8a38
André de Magalhães <arfddem@gmail.com>
2024-03-26 20:20:30 -0300

    commit -23

--- url-path-checker/main.go
+++ url-path-checker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/fs"
 	"net/http"
 	"os"
 	"strconv"
@@ -10,33 +11,40 @@ import (
 )
 
 const filePath = "./resources/urls.txt"
-const resultFileName = "testedResults.txt"
+const resultFileName = "./resources/testedResults.txt"
 const resultFilePermissions = 0666
 
 func main() {
-	readUrlsFromFile()
+	resultsFile := resultsFile(resultFileName, resultFilePermissions)
+	urls := readUrlsFromFile()
+	for index, url := range urls {
+		fmt.Println(index)
+		ping(url, resultsFile)
+	}
+
 }
 
-func readUrlsFromFile() {
+func readUrlsFromFile() []string {
 	file, err := os.Open(filePath)
+	var urls []string
 	if err != nil {
 		fmt.Println("There was an error while trying to read the requested file:")
 		fmt.Println("File Path: ", filePath)
 		fmt.Println("Error: ", err)
 	}
-	defer file.Close()
 	buf := []byte{}
 	scanner := bufio.NewScanner(file)
 	scanner.Buffer(buf, 2048*1024)
 	for scanner.Scan() {
-		ping(scanner.Text())
+		urls = append(urls, scanner.Text())
 	}
+	file.Close()
+	return urls
 }
 
-func ping(url string) {
+func ping(url string, resultsFile os.File) {
 	trimmedUrl := strings.TrimSpace(url)
 	response, error := http.Get(strings.TrimSpace(url))
-	resultsFile, _ := os.Open(resultFileName)
 	if error == nil && response.StatusCode == http.StatusOK || response.StatusCode == http.StatusUnauthorized {
 		fmt.Println(strconv.Itoa(response.StatusCode) + " Response on " + url + "\r\n")
 		responseURL := response.Request.URL.String()
@@ -47,10 +55,7 @@ func ping(url string) {
 		fmt.Println("Error Response on " + response.Request.URL.String() + "\r\n")
 		resultsFile.WriteString(trimmedUrl + ", " + "Error " + strconv.Itoa(response.StatusCode) + ", " + responseURL + "\r\n")
 	}
-	resultsFile.Close()
 }
-
-/*
 func resultsFile(fileName string, filePermission fs.FileMode) os.File {
 	os.Remove(fileName)
 	resultsFile, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, filePermission)
@@ -60,4 +65,3 @@ func resultsFile(fileName string, filePermission fs.FileMode) os.File {
 	}
 	return *resultsFile
 }
-*/
