1b1c8a49808978017e9c9ed59546d0a3c2c4e05b
André de Magalhães <arfddem@gmail.com>
2024-03-27 17:26:29 -0300

    Probably final update

--- url-path-checker/main.go
+++ url-path-checker/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const filePath = "./resources/urls.txt"
-const resultFileName = "./resources/testedResults.txt"
+const resultFileName = "./resources/testedResults.csv"
 const resultFilePermissions = 0666
 
 func main() {
@@ -22,6 +22,7 @@ func main() {
 	fmt.Println("All urls have been reached, around " + strconv.Itoa(len((resultsUrls))) + " entries")
 	fmt.Println("Starting to write csv...")
 	resultsFile := resultsFile(resultFileName, resultFilePermissions)
+	resultsFile.WriteString("URLs, hasRedirect, Actual Redirect" + "\r\n")
 	resultsFile.WriteString(strings.Join(resultsUrls, "\n"))
 	resultsFile.Close()
 	time.Sleep(5 * time.Second)
@@ -53,16 +54,16 @@ func pingUrls(urls []string) []string {
 		trimmedUrl := strings.TrimSpace(url)
 		response, error := http.Get(strings.TrimSpace(url))
 		if error != nil {
-			urlsResults = append(urlsResults, trimmedUrl+", "+"ERROR ON CALL "+"\r\n")
+			urlsResults = append(urlsResults, trimmedUrl+", "+"ERROR ON CALL ")
 		} else if error == nil && response.StatusCode == http.StatusOK || response.StatusCode == http.StatusUnauthorized {
-			fmt.Println(strconv.Itoa(response.StatusCode) + " Response on " + url + "\r\n")
+			fmt.Println(strconv.Itoa(response.StatusCode) + " Response on " + url)
 			responseURL := response.Request.URL.String()
 			hasRedirect := strconv.FormatBool(strings.Compare(trimmedUrl, responseURL) != 0)
-			urlsResults = append(urlsResults, trimmedUrl+", "+hasRedirect+", "+responseURL+"\r\n")
+			urlsResults = append(urlsResults, trimmedUrl+", "+hasRedirect+", "+responseURL)
 		} else {
 			responseURL := response.Request.URL.String()
-			fmt.Println("Error Response on " + response.Request.URL.String() + "\r\n")
-			urlsResults = append(urlsResults, trimmedUrl+", "+"Error "+strconv.Itoa(response.StatusCode)+", "+responseURL+"\r\n")
+			fmt.Println("Error Response on " + response.Request.URL.String())
+			urlsResults = append(urlsResults, trimmedUrl+", "+"Error "+strconv.Itoa(response.StatusCode)+", "+responseURL)
 		}
 	}
 	return urlsResults
